7b24b482683f3e4a9c38c1b6f14c64f7e932cafb
Jiahao Chen <jiahao@mit.edu>
2014-07-08 19:25:18 -0400

    Add columnwise modified Gram-Schmidt

--- src/qr.jl
+++ src/qr.jl
@@ -1,7 +1,7 @@
 #Iterative algorithms for orthogonalization
 
 import Base: Algorithm, qrfact!
-export cgs
+export cgs, cmgs
 
 #Type to store the output
 immutable QRPair{T}
@@ -24,7 +24,7 @@ typealias cgs ClassicalGramSchmidtAlg
 cgs() = cgs(nothing)
 
 function qrfact!(Q, parameters::ClassicalGramSchmidtAlg)
-    ncols = parameters.n===nothing ? size(A, 2) : parameters.n
+    ncols = parameters.n===nothing ? size(Q, 2) : parameters.n
     R=zeros(ncols, ncols)
     for k=1:ncols
         for i=1:k-1
@@ -41,5 +41,29 @@ end
 #Support qrfact!(A, cgs) call
 qrfact!(A, ::Type{ClassicalGramSchmidtAlg}) = qrfact!(A, cgs()) 
 
+#Columnwise modified Gram-Schmidt
+
+immutable ColumnwiseModifiedGramSchmidtAlg <: OrthogonalizationAlg
+    n::Union(Int, Nothing) #Number of Gram-Schmidt columns to compute
+end
+
+typealias cmgs ColumnwiseModifiedGramSchmidtAlg
+cmgs() = cmgs(nothing)
+
+function qrfact!(Q, parameters::ColumnwiseModifiedGramSchmidtAlg)
+    ncols = parameters.n===nothing ? size(Q, 2) : parameters.n
+    R=zeros(ncols, ncols)
+    for k=1:ncols
+	for i=1:k-1
+	    R[i,k] = Q[:,i]⋅Q[:, k]
+	    Q[:, k]-= R[i, k]*Q[:, i]
+	end
+	R[k,k] = norm(Q[:,k])
+	Q[:,k]/= R[k,k]
+    end
+    QRPair(Q, Triangular(R, :U))
+end
+#Support qrfact!(A, cmgs) call
+qrfact!(A, ::Type{ColumnwiseModifiedGramSchmidtAlg}) = qrfact!(A, cmgs()) 
 
 include("../test/qr.jl")
--- test/qr.jl
+++ test/qr.jl
@@ -1,13 +1,15 @@
 #Test orthogonalization methods
 using Base.Test
 
+debug = false
 m=n=4
 A=randn(m, n)
 
 QRhouseholder = qr(A, thin=false)
 
-QRcgs = qrfact!(A, cgs)
-
-@test norm(QRhouseholder[1] - QRcgs.Q) % 1 < m*n*eps()
-@test_approx_eq abs(QRhouseholder[2]) abs(QRcgs.R)
-
+for algorithm in (cgs, cmgs)
+	debug && println(algorithm)
+	QRgs = qrfact!(copy(A), algorithm)
+	@test norm(QRhouseholder[1] - QRgs.Q) - iround(norm(QRhouseholder[1] - QRgs.Q)) < m*n*eps()
+	@test_approx_eq abs(QRhouseholder[2]) abs(QRgs.R)
+end
