ace2f7932e040eabf851a5e45e9331d65aa86927
Jiahao Chen <jiahao@mit.edu>
2014-07-09 05:43:56 -0400

    Replace normalization lines in qrfact! with normalize!() call

--- src/qr.jl
+++ src/qr.jl
@@ -3,6 +3,49 @@
 import Base: Algorithm, qrfact!
 export cgs, cmgs
 
+#########################
+# Normalization methods #
+#########################
+
+abstract NormalizationAlg <: Algorithm
+normalize!(v) = normalize!(v, NORM_DEFAULT)
+
+#No normalization
+immutable norm_none <: NormalizationAlg end
+normalize!(v::AbstractVector, parameters::norm_none) = v, 1.0
+
+#Naive normalization
+immutable norm_naive <: NormalizationAlg end
+function normalize!(v::AbstractVector, parameters::norm_naive)
+    nrm = norm(v)
+    v/nrm, nrm
+end
+
+#Pythagorean normalization
+#Ref: doi:10.1007/s00211-006-0042-1
+immutable norm_pythag <: NormalizationAlg
+    u::AbstractVector
+end
+
+const NORM_DEFAULT = norm_naive() #Default is naive normalization
+
+#This looks a little weird, but if you don't call the Pythagorean algorithm with
+#any previously computed data, then the algorithm reduces to the naive one.
+#So the default constructor will simply reassign the normalization algorithm to
+#the naive one.
+norm_pythag() = norm_naive()
+
+function normalize!(v::AbstractVector, parameters::norm_pythag)
+    s = norm(v)
+    p = norm(parameters.u)
+    nrm = √(s-p)*√(s+p)
+    v/nrm, nrm
+end
+
+################################
+# Orthogonalization algorithms #
+################################
+
 #Type to store the output
 immutable QRPair{T}
     Q::Matrix{T}
@@ -18,9 +61,12 @@ abstract OrthogonalizationAlg <: Algorithm
 
 immutable ClassicalGramSchmidtAlg <: OrthogonalizationAlg
     n::Union(Int, Nothing) #Number of Gram-Schmidt columns to compute
+    normalize::NormalizationAlg
 end
 
 typealias cgs ClassicalGramSchmidtAlg
+cgs(n::Nothing) = cgs(n, NORM_DEFAULT)
+cgs(n::Integer) = cgs(int(n), NORM_DEFAULT)
 cgs() = cgs(nothing)
 
 function qrfact!(Q, parameters::ClassicalGramSchmidtAlg)
@@ -33,8 +79,7 @@ function qrfact!(Q, parameters::ClassicalGramSchmidtAlg)
         for i=1:k-1
             Q[:,k]-= R[i,k]*Q[:,i]
         end
-        R[k,k] = norm(Q[:,k])
-        Q[:,k]/= R[k,k]
+        Q[:,k], R[k,k] = normalize!(Q[:,k], parameters.normalize)
     end
     QRPair(Q, Triangular(R, :U))
 end
@@ -45,59 +90,27 @@ qrfact!(A, ::Type{ClassicalGramSchmidtAlg}) = qrfact!(A, cgs())
 
 immutable ColumnwiseModifiedGramSchmidtAlg <: OrthogonalizationAlg
     n::Union(Int, Nothing) #Number of Gram-Schmidt columns to compute
+    normalize::NormalizationAlg
 end
 
 typealias cmgs ColumnwiseModifiedGramSchmidtAlg
+cmgs(n::Nothing) = cmgs(n, NORM_DEFAULT)
+cmgs(n::Integer) = cmgs(int(n), NORM_DEFAULT)
 cmgs() = cmgs(nothing)
 
 function qrfact!(Q, parameters::ColumnwiseModifiedGramSchmidtAlg)
     ncols = parameters.n===nothing ? size(Q, 2) : parameters.n
     R=zeros(ncols, ncols)
     for k=1:ncols
-	for i=1:k-1
-	    R[i,k] = Q[:,i]⋅Q[:, k]
-	    Q[:, k]-= R[i, k]*Q[:, i]
-	end
-	R[k,k] = norm(Q[:,k])
-	Q[:,k]/= R[k,k]
+    	for i=1:k-1
+    	    R[i,k] = Q[:,i]⋅Q[:, k]
+    	    Q[:, k]-= R[i, k]*Q[:, i]
+    	end
+        Q[:,k], R[k,k] = normalize!(Q[:,k], parameters.normalize)
     end
     QRPair(Q, Triangular(R, :U))
 end
 #Support qrfact!(A, cmgs) call
 qrfact!(A, ::Type{ColumnwiseModifiedGramSchmidtAlg}) = qrfact!(A, cmgs()) 
-
-#Normalization methods
-abstract NormalizationAlg <: Algorithm
-normalize!(v) = normalize!(v, norm_naive) #Default is naive normalization
-
-#No normalization
-immutable norm_none <: NormalizationAlg end
-normalize!(v::AbstractVector, ::Type{norm_none}) = v, 1.0
-
-#Naive normalization
-immutable norm_naive <: NormalizationAlg end
-function normalize!(v::AbstractVector, ::Type{norm_naive})
-    nrm = norm(v)
-    v/nrm, nrm
-end
-
-#Pythagorean normalization
-#Ref: doi:10.1007/s00211-006-0042-1
-immutable norm_pythag <: NormalizationAlg
-    u::AbstractVector
-end
-
-#This looks a little weird, but if you don't call the Pythagorean algorithm with
-#any previously computed data, then the algorithm reduces to the naive one.
-#So the default constructor will simply reassign the normalization algorithm to
-#the naive one.
-norm_pythag() = norm_naive
-
-function normalize!(v::AbstractVector, parameters::norm_pythag)
-    s = norm(v)
-    p = norm(parameters.u)
-    nrm = √(s-p)*√(s+p)
-    v/nrm, nrm
-end
 
 include("../test/qr.jl")
