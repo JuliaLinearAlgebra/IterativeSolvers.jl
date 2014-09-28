60fe471545e7a905ebc10053c6aee11b6bf2823d
Jiahao Chen <jiahao@mit.edu>
2014-09-28 17:11:06 -0400

    Tests from the Matrix Market

---
+++ test/matrixmarket.jl
@@ -0,0 +1,34 @@
+#Tests using the NIST Matrix Market
+
+Pkg.clone("https://github.com/dpo/HarwellBoeing.jl.git")
+
+using IterativeSolvers, MatrixMarket
+
+for (setname, matname) in [("cylshell", "s3dkq4m2")]
+    dl_filename = string(setname, "_", filename)
+    isfile(dl_filename) || download("ftp://math.nist.gov/pub/MatrixMarket2/misc/$setname/$matname.mtx.gz", dl_filename)
+
+    vars = matread(dl_filename)
+    A, b = vars["Problem"]["A"], vars["Problem"]["b"][:]
+
+    #Use direct solve as benchmark
+    println("$dl_filename: Direct solve")
+    tic()
+    x = A \ b
+    toc()
+
+    @show Aissym = issym(A)
+    @show Aisposdef = isposdef(A)
+    solvers = {}
+    Aissym && Aisposdef && push!(solvers, cg)
+    push!(solvers, gmres)
+    for solver in solvers
+        println("$dl_filename: Iterative solve using $solver")
+        tic()
+        xs, ch = solver(A, b)
+        toc()
+	r=norm(x-xs)
+        println("Residual: $r matvecs: $(ch.mvps) iters: $(length(ch.residuals))")
+    end
+end
+
