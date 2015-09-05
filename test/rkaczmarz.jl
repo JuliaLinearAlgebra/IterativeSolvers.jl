1b1419d5209add2be68cf8967136386f52f2d71b
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-09-05 09:23:03 -0400

    add convergence history

--- test/rkaczmarz.jl
+++ test/rkaczmarz.jl
@@ -3,14 +3,14 @@ using IterativeSolvers, Base.Test
 tol = 1e-10
 N=10
 M = 100
-A = randn(M,N)
+A = sprandn(M,N, 0.2)
 x = randn(N)
 rhs = A*x
 
 # sparse matrix
 (x, converged) = rkaczmarz(A',rhs, tol = tol)
-@test norm(A*x - rhs, Inf) <= tol
+@test norm(A*x - rhs) <= tol
 
 # full matrix
 (x, converged) = rkaczmarz(full(A'),rhs, tol = tol)
-@test norm(A*x - rhs, Inf) <= tol
+@test norm(A*x - rhs) <= tol
