f7530a4a3d2993c6d56cd9aa18e7cb41fffff003
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-09-26 16:18:14 -0400

    write new method rkaczmarzT. Check norm2 > 0

--- src/rkaczmarz.jl
+++ src/rkaczmarz.jl
@@ -5,31 +5,35 @@ export rkaczmarz, rkaczmarz!
 ##
 ## "A randomized Kaczmarz algorithm with exponential convergence"
 ## Thomas Strohmer and Roman Vershynin
-## At must be the transpose of A in Ax = b
-## 
+## rkaczmarzT takes At as first argument to solve Ax = b
 ##############################################################################
 
-function rkaczmarz(At, b; tol::Real=1e-10, maxiter::Integer = 100_000)
-    T = Adivtype(At, b)
-    x = zeros(T, size(At, 1))
-    rkaczmarz!(x, At, b; tol = tol, maxiter = maxiter)
+function rkaczmarz(A, b ; kwargs...)
+    rkaczmarzT(A', b ; kwargs...)
 end
 
-function rkaczmarz!(x, At, b; tol::Real=1e-10, maxiter::Integer = 100_000)
+function rkaczmarzT(At, b; kwargs...)
+    T = Adivtype(At, b)
+    x = zeros(T, size(At, 1))
+    rkaczmarzT!(x, At, b; kwargs...)
+end
 
+function rkaczmarzT!(x, At, b; tol::Real=1e-10, maxiter::Integer = 100_000)
     # checks
     length(x) == size(At, 1) || error("The number of rows in At is $(size(At, 1)) but the length of x is $(length(x))")
     length(b) == size(At, 2) || error("The number of columns in At is $(size(At, 2)) but the length of b is $(length(b))")
 
     # initialize
     Tx = eltype(x)
-    ch = ConvergenceHistory(false, tol, 0, Tx[])
+    ssrs = Tx[]
 
     # precompute norm (distribution) and invnorm = 1/norm[i] 
     len_b = length(b)
     norm2 = _norm2cols(At)
     invnorm2 = 1 ./ norm2
-    p = scale!(norm2, 1/sum(norm2))
+    @show any(x -> x == 0, norm2)
+    any(x -> x == 0, norm2) && error("Matrix is singular")
+    p = scale!(norm2, inv(sum(norm2)))
    	distribution = Categorical(p)
     res = b - At_mul_B(At, x)
     iter = 0
@@ -44,14 +48,14 @@ function rkaczmarz!(x, At, b; tol::Real=1e-10, maxiter::Integer = 100_000)
             _update!(x, At, i, α)
         end
 
-        # check convergence every length(b)
+        # check convergence every length(b) iteration
         ssr = norm(res)
-        push!(ch, ssr)
-        if ssr < tol
-            ch.isconverged = true
+        push!(ssrs, ssr)
+        if ssr < tol^2
+            break
         end
     end
-    return x, ch
+    return x, ConvergenceHistory(ssrs[end] < tol, tol, length(ssrs), ssrs)
 end
 
 
@@ -60,12 +64,6 @@ end
 ## Sparse Matrix
 ##
 ##############################################################################
-
-if VERSION < v"0.4.0-dev+1307" 
-    rowvals(S::Base.SparseMatrixCSC) = S.rowval
-    nzrange(S::Base.SparseMatrixCSC, col::Integer) = S.colptr[col]:(S.colptr[col+1]-1)
-end
-
 function _norm2cols{Tv, Ti}(At::SparseMatrixCSC{Tv, Ti})
     Tv[sumabs2(sub(nonzeros(At),  nzrange(At, i))) for i in 1:size(At, 2)]
 end
@@ -94,7 +92,6 @@ end
 ## Full Matrix
 ##
 ##############################################################################
-
 _norm2cols(At::Matrix) = vec(sumabs2(At, 1))
 
 _dot(x, At::Matrix, i::Integer) = dot(x, slice(At, :, i))
--- test/rkaczmarz.jl
+++ test/rkaczmarz.jl
@@ -1,16 +1,15 @@
 using IterativeSolvers, Base.Test
 
 tol = 1e-10
-N=10
+N = 10
 M = 100
-A = sprandn(M,N, 0.2)
-x = randn(N)
-rhs = A*x
+A = sprandn(M, N, 0.2)
+b = randn(M)
 
 # sparse matrix
-(x, converged) = rkaczmarz(A',rhs, tol = tol)
-@test norm(A*x - rhs) <= tol
+(x, converged) = rkaczmarz(A'A, A'b, tol = tol)
+@test norm(A'A*x - A'b) <= tol
 
 # full matrix
-(x, converged) = rkaczmarz(full(A'),rhs, tol = tol)
-@test norm(A*x - rhs) <= tol
+(x, converged) = rkaczmarz(full(A'A), A'b, tol = tol)
+@test norm(A'A*x - A' * b) <= tol
