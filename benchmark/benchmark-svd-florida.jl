4ee28fe01c9c9720078023de053e12f42cee2575
Jiahao Chen (陳家豪) <jiahao@mit.edu>
2015-10-14 23:15:01 -0400

    Fix up benchmarks to work on master of Benchmarks.jl
    Also refactor benchmark driver so that the tests on a single matrix are
    in a separate function.

--- benchmark/benchmark-svd-florida.jl
+++ benchmark/benchmark-svd-florida.jl
@@ -2,8 +2,17 @@
 #
 # Benchmarking script for singular value problems
 
-#Pkg.clone("https://github.com/johnmyleswhite/Benchmarks.jl")
-#Pkg.clone("https://github.com/andreasnoack/PROPACK.jl")
+try
+    Pkg.installed("Benchmarks")
+catch
+    Pkg.clone("https://github.com/johnmyleswhite/Benchmarks.jl")
+end
+
+try
+    Pkg.installed("PROPACK")
+catch
+    Pkg.clone("https://github.com/andreasnoack/PROPACK.jl")
+end
 
 using Benchmarks
 using IterativeSolvers
@@ -13,114 +22,135 @@ using PROPACK
 
 BASEDIR = "florida"
 
-for group in readdir(BASEDIR)
-    isdir(joinpath(BASEDIR, group)) || continue
-    for matrix in readdir(joinpath(BASEDIR, group))
-        filename = joinpath(BASEDIR, group, matrix)
+if !isdir(BASEDIR)
+    info("Setting up $BASEDIR collection")
+    include("setup-florida.jl")
+end
 
-        endswith(matrix, ".mat") || continue
+"""
+Run benchmark function `runbenchmark` against all .mat files in all
+subdirectories of BASEDIR
+"""
+function benchmarkall(BASEDIR)
+    for group in readdir(BASEDIR)
+        isdir(joinpath(BASEDIR, group)) || continue
+        for matrix in readdir(joinpath(BASEDIR, group))
+            filename = joinpath(BASEDIR, group, matrix)
 
-	#If we already saved benchmarks, don't rerun
-	benchmarkfilename = joinpath(BASEDIR, group, matrix[1:end-3]*"jld")
-	isfile(benchmarkfilename) && continue
+            endswith(matrix, ".mat") || continue
 
-	#To debug this script, it's useful to run it on small matrices only
-	filesize(filename) < 100000 || continue
+        #If we already saved benchmarks, don't rerun
+        benchmarkfilename = joinpath(BASEDIR, group, matrix[1:end-3]*"jld")
+        isfile(benchmarkfilename) && continue
 
-        mf = matread(filename)
-        if !haskey(mf, "Problem")
-            warn("Skipping unknown file $filename: No 'Problem' struct")
-            continue
+        #To debug this script, it's useful to run it on small matrices only
+        #filesize(filename) < 100000 || continue
+
+        runbenchmark(filename, benchmarkfilename)
+
+        #debug: just try one
+        #@goto stop
         end
+    end
+    @label stop
+end
 
-        pr = mf["Problem"]
-        if !haskey(pr, "A")
-            warn("Skipping unknown file $filename: 'Problem' struct has no matrix 'A'")
-            continue
+
+"""
+Run several different computations for singular values
+"""
+function runbenchmark(filename, benchmarkfilename)
+    mf = matread(filename)
+    if !haskey(mf, "Problem")
+        warn("Skipping unknown file $filename: No 'Problem' struct")
+        return
+    end
+
+    pr = mf["Problem"]
+    if !haskey(pr, "A")
+        warn("Skipping unknown file $filename: 'Problem' struct has no matrix 'A'")
+        return
+    end
+    A = pr["A"]
+    #eltype(A) <: Real || warn("Skipping matrix with unsupported element type $(eltype(A))")
+    info(filename*", matrix of type $(typeof(A)) and size $(size(A))")
+    m, n = size(A)
+    #Choose the same normalized unit vector to start with
+    q = randn(n)
+    eltype(A) <: Complex && (q += im*randn(n))
+    scale!(q, inv(norm(q)))
+    qm = randn(m)
+    eltype(A) <: Complex && (q += im*randn(m))
+    scale!(qm, inv(norm(qm)))
+
+    #Number of singular values to request
+    nv = min(m, n, 10)
+
+    #Maximum number of iterations
+    maxiter = max(m, n)
+
+    #Tolerance, however the algorithm chooses to interpret this
+    #Set convergence criterion to sqrt eps
+    tol = √eps(real(one(eltype(A))))
+
+    info("svds (eigs on [0 A; A' 0])")
+    #See #12890 :(
+    #b_svds = @benchmark svds(A, v0=q, nsv=nv, tol=tol, maxiter=maxiter)
+    b_svds = @benchmark svds(A, nsv=nv, tol=tol, maxiter=maxiter)
+
+    info("eigs on A'A or AA'")
+    makeata(A) = (size(A, 1)≥ size(A,2) ? A*A' : A'A)
+    B = makeata(A)
+    b_ata = @benchmark makeata(A)
+    b_eigs = if m≥n
+        B = A'A #Size n x n
+        b_eigs = @benchmark eigs(B, v0=q, nev=nv, tol=tol, maxiter=maxiter)
+    else
+        B = A*A' #Size m x m
+        #eigs cannot use the same starting vector as everyone else, needs R^m not R^n
+        b_eigs = @benchmark eigs(B, v0=qm, nev=nv, tol=tol, maxiter=maxiter)
+    end
+
+    info("tsvd (PROPACK)")
+    b_tsvd = try
+        if m≥n #PROPACK implicitly assumes the input is tall
+            @benchmark tsvdvals(A, initvec=qm, kmax=maxiter, k=nv, tolin=tol)
+        else
+            @benchmark tsvdvals(A', initvec=q, kmax=maxiter, k=nv, tolin=tol)
         end
-        A = pr["A"]
+    catch exc
+        println("Exception: $exc")
+    end
 
-        #eltype(A) <: Real || warn("Skipping matrix with unsupported element type $(eltype(A))")
-        info(filename*", matrix of type $(typeof(A)) and size $(size(A))")
-	m, n = size(A)
-	#Choose the same normalized unit vector to start with
-	q = randn(n)
-        eltype(A) <: Complex && (q += im*randn(n))
-        scale!(q, inv(norm(q)))
-	qm = randn(m)
-        eltype(A) <: Complex && (q += im*randn(m))
-        scale!(qm, inv(norm(qm)))
+    info("GKL with thick restart using Ritz values")
+    b_tr = try
+        @benchmark svdl(A, nv, v0=q, tol=tol, reltol=tol)
+    catch exc
+        println("Exception: $exc")
+    end
 
-        #Number of singular values to request
-        nv = min(m, n, 10)
-
-        #Maximum number of iterations
-        maxiter = max(m, n)
-
-        #Tolerance, however the algorithm chooses to interpret this
-	#Set convergence criterion to sqrt eps
-	tol = √eps(real(one(eltype(A))))
-
-        info("svds (eigs on [0 A; A' 0])")
-	#See #12890 :(
-	#b_svds = @benchmark svds(A, v0=q, nsv=nv, tol=tol, maxiter=maxiter)
-	b_svds = @benchmark svds(A, nsv=nv, tol=tol, maxiter=maxiter)
-
-	info("eigs on A'A or AA'")
-	b_ata = @benchmark B = m≥n ? A*A' : A'A
-	b_eigs = if m≥n
-	    B = A'A #Size n x n
-	    b_eigs = @benchmark eigs(B, v0=q, nev=nv, tol=tol, maxiter=maxiter)
-	else
-	    B = A*A' #Size m x m
-	    #eigs cannot use the same starting vector as everyone else, needs R^m not R^n
-	    b_eigs = @benchmark eigs(B, v0=qm, nev=nv, tol=tol, maxiter=maxiter)
-	end
-
-	info("tsvd (PROPACK)")
-	b_tsvd = try
-	    if m≥n #PROPACK implicitly assumes the input is tall
-	        @benchmark tsvdvals(A, initvec=qm, kmax=maxiter, k=nv, tolin=tol)
-	    else
-	        @benchmark tsvdvals(A', initvec=q, kmax=maxiter, k=nv, tolin=tol)
-            end
-        catch exc
-            println("Exception: $exc")
-        end
-
-        info("GKL with thick restart using Ritz values")
-        b_tr = try
-            @benchmark svdl(A, nv, v0=q, tol=tol, reltol=tol)
-        catch exc
-            println("Exception: $exc")
-        end
-
-	info("GKL with thick restart using harmonic Ritz values")
+    info("GKL with thick restart using harmonic Ritz values")
         b_trh = try
             @benchmark svdl(A, nv, v0=q, tol=tol, reltol=tol, method=:harmonic)
         catch exc
             println("Exception: $exc")
         end
 
-	data = Dict(
-	    "svds" => b_svds,
-	    "ata"  => b_ata,
-	    "eigs" => b_eigs,
-	    "tsvd" => b_tsvd,
-	    "tr"   => b_tr,
-	    "trh"  => b_trh
-	)
-	println("Timings:")
-	for (k, v) in data
-	    println(k)
-	    display(v)
-	    println()
-        end
-        JLD.save(benchmarkfilename, data, compress=true)
-
-	#debug: just try one
-	#@goto stop
+    data = Dict(
+        "svds" => b_svds,
+        "ata"  => b_ata,
+        "eigs" => b_eigs,
+        "tsvd" => b_tsvd,
+        "tr"   => b_tr,
+        "trh"  => b_trh
+    )
+    println("Timings:")
+    for (k, v) in data
+        println(k)
+        display(v)
+        println()
     end
+    JLD.save(benchmarkfilename, data, compress=true)
 end
-@label stop
 
+benchmarkall(BASEDIR)
--- benchmark/setup-florida.jl
+++ benchmark/setup-florida.jl
@@ -3,21 +3,21 @@
 #Benchmark iterative methods against matrices from the University of Florida
 #sparse collection
 
-VERSION ≥ v"0.4-" || error("Julia 0.4 required")
+VERSION ≥ v"0.4" || error("Julia 0.4 required")
 
 #Root URL to the matrix collection
-const UFL_URL_ROOT = "http://www.cise.ufl.edu/research/sparse/matrices"
+UFL_URL_ROOT = "http://www.cise.ufl.edu/research/sparse/matrices"
 #Plain text file containing list of matrices
-const MASTERLIST = "matrices.txt"
+MASTERLIST = "matrices.txt"
 #Download UFL collection to this directory
-const BASEDIR = "florida"
+BASEDIR = "florida"
 
 # 1. Read in master list of matrices from BASEDIR/MASTERLIST
 #    If absent, generate this file. Requires Gumbo.jl
 Pkg.installed("Gumbo")==nothing || using Gumbo
 isdir(BASEDIR) || mkdir(BASEDIR)
 if !isfile(joinpath(BASEDIR, MASTERLIST))
-    Pkg.installed("Gumbo")==nothing && error("Parsing list from UFL website requires Gumbo.jl")
+    Pkg.installed("Gumbo")===nothing && error("Parsing list from UFL website requires Gumbo.jl")
     #Parse the list on the UFL website. Requires the Gumbo HTML parser
 
     dlfilename = joinpath(BASEDIR, "florida.html")
