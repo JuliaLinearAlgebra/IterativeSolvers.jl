79acb0e701ba788ccd8065a9dbbf63b5a8da8ed9
Andreas Noack <andreasnoackjensen@gmail.com>
2015-10-23 10:45:00 -0400

    Fix tolerance in lanczos such that it is generic on element type

--- src/lanczos.jl
+++ src/lanczos.jl
@@ -18,7 +18,7 @@ function lanczos!{T}(K::KrylovSubspace{T})
     SymTridiagonal(αs, βs)
 end
 
-function eigvals_lanczos(A, neigs::Int=size(A,1); tol::Real=size(A,1)^3*eps(), maxiter::Int=size(A,1))
+function eigvals_lanczos(A, neigs::Int=size(A,1); tol::Real = size(A,1)^3*eps(real(eltype(A))), maxiter::Integer = size(A,1))
     K = KrylovSubspace(A, size(A, 1), 2) #In Lanczos, only remember the last two vectors
     initrand!(K)
     resnorms = zeros(maxiter)
