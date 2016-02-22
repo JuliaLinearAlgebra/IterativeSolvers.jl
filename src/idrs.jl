6f4df86ddfc2716afca74d1d052d0b5c83bcfa00
RomainBrault <ro.brault@gmail.com>
2016-02-22 12:25:52 +0100

    added residual smoothing as in the python code https://github.com/astudillor/idrs_py/blob/master/idrs.py

--- src/idrs.jl
+++ src/idrs.jl
@@ -1,3 +1,4 @@
+import Base.LinAlg.BLAS: axpy!
 export idrs, idrs!
 
 ####
@@ -24,9 +25,9 @@ IDRsSolver.jl
 The Induced Dimension Reduction method is a family of simple and fast Krylov
 subspace algorithms for solving large nonsymmetric linear systems. The idea
 behind the IDR(s) variant is to generate residuals that are in the nested
-subspaces of shrinking dimension s. 
+subspaces of shrinking dimension s.
 
-        
+
 
 Syntax
 ------
@@ -39,57 +40,66 @@ Syntax
 
 Arguments
 ---------
-       
-       s -- dimension reduction number. Normally, a higher s gives faster convergence, 
-            but also  makes the method more expensive.
-       tol -- tolerance of the method.  
-       maxiter -- maximum number of iterations
 
-       
+       s -- dimension reduction number. Normally, a higher s gives faster convergence,
+            but also  makes the method more expensive.
+       tol -- tolerance of the method.
+       maxiter -- maximum number of iterations
+       smoothing -- (Logical) specifies if residual smoothing must be applied. Default is false.
+
+
 Output
 ------
 
         X -- Approximated solution by IDR(s)
         h -- Convergence history
-        
 
-        The [`ConvergenceHistory`](https://github.com/JuliaLang/IterativeSolvers.jl/issues/6) type provides information about the iteration history. 
+
+        The [`ConvergenceHistory`](https://github.com/JuliaLang/IterativeSolvers.jl/issues/6) type provides information about the iteration history.
             - `isconverged::Bool`, a flag for whether or not the algorithm is converged.
             - `threshold`, the convergence threshold
-            - `residuals::Vector`, the value of the convergence criteria at each iteration        
-        
-        
+            - `residuals::Vector`, the value of the convergence criteria at each iteration
 
-    
+
+
+
 References
 ----------
 
-    [1] IDR(s): a family of simple and fast algorithms for solving large 
+    [1] IDR(s): a family of simple and fast algorithms for solving large
         nonsymmetric linear systems. P. Sonneveld and M. B. van Gijzen
-        SIAM J. Sci. Comput. Vol. 31, No. 2, pp. 1035--1062, 2008 
-    [2] Algorithm 913: An Elegant IDR(s) Variant that Efficiently Exploits 
+        SIAM J. Sci. Comput. Vol. 31, No. 2, pp. 1035--1062, 2008
+    [2] Algorithm 913: An Elegant IDR(s) Variant that Efficiently Exploits
         Bi-orthogonality Properties. M. B. van Gijzen and P. Sonneveld
         ACM Trans. Math. Software,, Vol. 38, No. 1, pp. 5:1-5:19, 2011
     [3] This file is a translation of the following MATLAB implementation:
         http://ta.twi.tudelft.nl/nw/users/gijzen/idrs.m
     [4] IDR(s)' webpage http://ta.twi.tudelft.nl/nw/users/gijzen/IDR.html
 """
-idrs(A, b; s = 8, tol=sqrt(eps(typeof(real(b[1])))), maxiter = length(b)^2) =
-    idrs_core!(zerox(A,b), linsys_op, (A,), b, s, tol, maxiter)
+idrs(A, b; s = 8, tol=sqrt(eps(typeof(real(b[1])))), maxiter = length(b)^2, smoothing=false) =
+    idrs_core!(zerox(A, b), linsys_op, (A,), b, s, tol, maxiter; smoothing=smoothing)
 
-idrs!(x, A, b; s = 8, tol=sqrt(eps(typeof(real(b[1])))), maxiter=length(x)^2) =
-    idrs_core!(x, linsys_op, (A,), b, s, tol, maxiter)
+idrs!(x, A, b; s = 8, tol=sqrt(eps(typeof(real(b[1])))), maxiter=length(x)^2, smoothing=false) =
+    idrs_core!(x, linsys_op, (A,), b, s, tol, maxiter; smoothing=smoothing)
 
-function idrs_core!{T}(X, op, args, C::T, s, tol, maxiter)
+function idrs_core!{T}(X, op, args, C::T,
+    s::Number, tol::Number, maxiter::Number; smoothing::Bool=false)
+
     R = C - op(X, args...)::T
     normR = vecnorm(R)
     res = typeof(tol)[normR]
-	iter = 0                 
+	iter = 0
+
+    if smoothing
+        X_s = copy(X)
+        R_s = copy(R)
+        T_s = zeros(R)
+    end
 
     if normR <= tol           # Initial guess is a good enough solution
         return X, ConvergenceHistory(0<= res[end] < tol, tol, length(res), res)
     end
-    
+
     Z = zero(C)
 
     P = T[rand!(copy(C)) for k in 1:s]
@@ -97,25 +107,24 @@ function idrs_core!{T}(X, op, args, C::T, s, tol, maxiter)
     G = T[copy(Z) for k in 1:s]
     Q = copy(Z)
     V = copy(Z)
-    
+
     M = eye(eltype(C),s,s)
     f = zeros(eltype(C),s)
     c = zeros(eltype(C),s)
 
     om::eltype(C) = 1
-    iter = 0
     while normR > tol && iter < maxiter
         for i in 1:s,
             f[i] = vecdot(P[i], R)
         end
-        for k in 1:s 
+        for k in 1:s
 
             # Solve small system and make v orthogonal to P
 
             c = LowerTriangular(M[k:s,k:s])\f[k:s]
             copy!(V, G[k])
             scale!(c[1], V)
-        
+
             copy!(Q, U[k])
             scale!(c[1], Q)
             for i = k+1:s
@@ -128,10 +137,10 @@ function idrs_core!{T}(X, op, args, C::T, s, tol, maxiter)
             #V = R - V
             scale!(-1., V)
             axpy!(1., R, V)
-            
+
             copy!(U[k], Q)
             axpy!(om, V, U[k])
-            G[k] = op(U[k], args...) 
+            G[k] = op(U[k], args...)
 
             # Bi-orthogonalise the new basis vectors
 
@@ -144,40 +153,67 @@ function idrs_core!{T}(X, op, args, C::T, s, tol, maxiter)
             # New column of M = P'*G  (first k-1 entries are zero)
 
             for i in k:s
-                M[i,k] = vecdot(P[i],G[k])       
+                M[i,k] = vecdot(P[i],G[k])
             end
 
-            #  Make r orthogonal to q_i, i = 1..k 
+            #  Make r orthogonal to q_i, i = 1..k
 
             beta = f[k]/M[k,k]
             axpy!(-beta, G[k], R)
             axpy!(beta, U[k], X)
-        
+
             normR = vecnorm(R)
+            if smoothing
+                # T_s = R_s - R
+                copy!(T_s, R_s); axpy!(-1., R, T_s)
+
+                gamma = vecdot(R_s, T_s)/vecdot(T_s, T_s)
+
+                # R_s = R_s - gamma*T_s
+                axpy!(-gamma, T_s, R_s)
+                # X_s = X_s - gamma*(X_s - X)
+                copy!(T_s, X_s); axpy!(-1., X, T_s); axpy!(-gamma, T_s, X_s)
+
+                normR = vecnorm(R_s)
+            end
             res = [res; normR]
             iter += 1
             if normR < tol || iter > maxiter
                 return X, ConvergenceHistory(normR < tol, tol, length(res), res)
-            end 
-            if k < s 
+            end
+            if k < s
                 f[k+1:s] = f[k+1:s] - beta*M[k+1:s,k]
             end
-        
+
         end
 
         # Now we have sufficient vectors in G_j to compute residual in G_j+1
         # Note: r is already perpendicular to P so v = r
-
         copy!(V, R)
         Q = op(V, args...)::T
         om = omega(Q, R)
         axpy!(-om, Q, R)
         axpy!(om, V, X)
 
-        
         normR = vecnorm(R)
+        if smoothing
+            # T_s = R_s - R
+            copy!(T_s, R_s); axpy!(-1., R, T_s)
+
+            gamma = vecdot(R_s, T_s)/vecdot(T_s, T_s)
+
+            # R_s = R_s - gamma*T_s
+            axpy!(-gamma, T_s, R_s)
+            # X_s = X_s - gamma*(X_s - X)
+            copy!(T_s, X_s); axpy!(-1., X, T_s); axpy!(-gamma, T_s, X_s)
+
+            normR = vecnorm(R_s)
+        end
         iter += 1
         res = [res; normR]
+    end
+    if smoothing
+        copy!(X, X_s)
     end
     return X, ConvergenceHistory(res[end]<tol, tol, length(res), res)
 end
