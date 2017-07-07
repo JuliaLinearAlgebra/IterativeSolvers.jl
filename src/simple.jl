ccf5d85ebf2360454b519f2bf4b70ecca6c5240b
Harmen Stoppels <harmenstoppels@gmail.com>
2017-07-07 19:16:22 +0200

    Power method as iterator

--- src/simple.jl
+++ src/simple.jl
@@ -1,3 +1,5 @@
+import Base: start, next, done
+
 #Simple methods
 export powm, invpowm
 
@@ -5,91 +7,109 @@ export powm, invpowm
 # API method calls #
 ####################
 
-function powm(A;
-    x=nothing, tol::Real=eps(real(eltype(A)))*size(A,2)^3, maxiter::Int=size(A,2),
-    log::Bool=false, kwargs...
-    )
-    K = KrylovSubspace(A, 1)
-    x==nothing ? initrand!(K) : init!(K, x/norm(x))
+type PowerMethodIterable{matT, vecT <: AbstractVector, numT <: Number, eigvalT <: Number}
+    A::matT
+    x::vecT
+    tol::numT
+    maxiter::Int
+    θ::eigvalT
+    r::vecT
+    Ax::vecT
+    residual::numT
+end
 
-    history = ConvergenceHistory(partial=!log)
+
+##
+## Iterators
+##
+
+@inline converged(p::PowerMethodIterable) = p.residual < p.tol
+
+@inline start(p::PowerMethodIterable) = 0
+
+@inline done(p::PowerMethodIterable, iteration::Int) = iteration > p.maxiter || converged(p)
+
+function next(p::PowerMethodIterable, iteration::Int)
+
+    A_mul_B!(p.Ax, p.A, p.x)
+
+    # Rayleigh quotient θ = x'Ax
+    p.θ = dot(p.x, p.Ax)
+
+    # (Previous) residual vector r = Ax - λx
+    copy!(p.r, p.Ax)
+    BLAS.axpy!(-p.θ, p.x, p.r)
+
+    # Normed residual
+    p.residual = norm(p.r)
+
+    # Normalize the next approximation
+    copy!(p.x, p.Ax)
+    scale!(p.x, one(eltype(p.x)) / norm(p.x))
+
+    p.residual, iteration + 1
+end
+
+# Transforms the eigenvalue back whether shifted or inversed
+@inline transform_eigenvalue(θ, inverse::Bool, σ) = σ + (inverse ? inv(θ) : θ)
+
+function powm_iterable!(A, x; tol = eps(real(eltype(A))) * size(A, 2) ^ 3, maxiter = size(A, 1))
+    T = eltype(x)
+    PowerMethodIterable(A, x, tol, maxiter, zero(T), similar(x), similar(x), realmax(real(T)))
+end
+
+function powm_iterable(A; kwargs...)
+    x0 = rand(Complex{real(eltype(A))}, size(A, 1))
+    scale!(x0, 1.0 / norm(x0))
+    powm_iterable!(A, x0; kwargs...)
+end
+
+function powm(A; kwargs...)
+    x0 = rand(Complex{real(eltype(A))}, size(A, 1))
+    scale!(x0, 1.0 / norm(x0))
+    powm!(A, x0; kwargs...)
+end
+
+function powm!(A, x;
+    tol = eps(real(eltype(A))) * size(A, 2) ^ 3,
+    maxiter = size(A, 1),
+    shift = zero(eltype(A)),
+    inverse::Bool = false,
+    log::Bool = false,
+    verbose::Bool = false
+)
+    history = ConvergenceHistory(partial = !log)
     history[:tol] = tol
-    reserve!(history,:resnorm, maxiter)
-    eig, v = powm_method!(history, K; tol=tol, maxiter=maxiter, kwargs...)
-    log && shrink!(history)
-    log ? (eig, v, history) : (eig, v)
-end
+    reserve!(history, :resnorm, maxiter)
+    verbose && @printf("=== powm ===\n%4s\t%7s\n", "iter", "resnorm")
 
-#########################
-# Method Implementation #
-#########################
+    iterable = powm_iterable!(A, x, tol = tol, maxiter = maxiter)
 
-function powm_method!{T}(log::ConvergenceHistory, K::KrylovSubspace{T};
-    tol::Real=eps(real(T))*K.n^3, maxiter::Int=K.n, verbose::Bool=false
-    )
-    verbose && @printf("=== powm ===\n%4s\t%7s\n","iter","resnorm")
-    θ = zero(T)
-    v = Array(T, K.n)
-    for iter=1:maxiter
-        nextiter!(log,mvps=1)
-        v = lastvec(K)
-        y = nextvec(K)
-        θ = dot(v, y)
-        resnorm = real(norm(y - θ*v))
-        push!(log, :resnorm, resnorm)
-        verbose && @printf("%3d\t%1.2e\n",iter,resnorm)
-        resnorm <= tol*abs(θ) && (setconv(log, resnorm >= 0); break)
-        appendunit!(K, y)
+    for (iteration, residual) = enumerate(iterable)
+        nextiter!(history, mvps = 1)
+        verbose && @printf("%3d\t%1.2e\n", iteration, residual)
     end
-    verbose && @printf("\n")
-    θ, v
-end
 
-####################
-# API method calls #
-####################
+    setconv(history, converged(iterable))
 
-function invpowm(A;
-    x=nothing, shift::Number=0, tol::Real=eps(real(eltype(A)))*size(A,2)^3,
-    maxiter::Int=size(A,2), log::Bool=false, kwargs...
-    )
-    K = KrylovSubspace(A, 1)
-    x==nothing ? initrand!(K) : init!(K, x/norm(x))
+    println()
 
-    history = ConvergenceHistory(partial=!log)
-    history[:tol] = tol
-    reserve!(history,:resnorm, maxiter)
-    eig, v = invpowm_method!(history, K, shift; tol=tol, maxiter=maxiter, kwargs...)
     log && shrink!(history)
-    log ? (eig, v, history) : (eig, v)
+
+    λ = transform_eigenvalue(iterable.θ, inverse, shift)
+    x = iterable.x
+
+    log ? (λ, x, history) : (λ, x)
 end
 
-#########################
-# Method Implementation #
-#########################
 
-function invpowm_method!{T}(log::ConvergenceHistory, K::KrylovSubspace{T}, σ::Number=zero(T);
-    tol::Real=eps(real(T))*K.n^3, maxiter::Int=K.n, verbose::Bool=false
-    )
-    verbose && @printf("=== invpowm ===\n%4s\t%7s\n","iter","resnorm")
-    θ = zero(T)
-    v = Array(T, K.n)
-    y = Array(T, K.n)
-    σ = convert(T, σ)
-    for iter=1:maxiter
-        nextiter!(log,mvps=1)
-        v = lastvec(K)
-        y = (K.A-σ*eye(K))\v
-        θ = dot(v, y)
-        resnorm = norm(y - θ*v)
-        push!(log, :resnorm, resnorm)
-        verbose && @printf("%3d\t%1.2e\n",iter,resnorm)
-        resnorm <= tol*abs(θ) && (setconv(log, resnorm >= 0); break)
-        appendunit!(K, y)
-    end
-    verbose && @printf("\n")
-    σ+1/θ, y/θ
+function invpowm(A; kwargs...)
+    x0 = rand(Complex{real(eltype(A))}, size(A, 1))
+    scale!(x0, 1.0 / norm(x0))
+    invpowm!(A, x0; kwargs...)
 end
+
+invpowm!(A, x0; kwargs...) = powm!(A, x0; inverse = true, kwargs...)
 
 #################
 # Documentation #
--- test/simple_eigensolvers.jl
+++ test/simple_eigensolvers.jl
@@ -10,31 +10,42 @@ srand(1234321)
 #######################
 
 facts("simple eigensolvers") do
-for T in (Float32, Float64, Complex64, Complex128)
-    context("Matrix{$T}") do
-    A=convert(Matrix{T}, randn(n,n))
-    T<:Complex && (A+=convert(Matrix{T}, im*randn(n,n)))
-    A=A+A' #Symmetric/Hermitian
 
-    tol = (eltype(T) <: Complex ?2:1)*n^2*cond(A)*eps(real(one(T)))
+n = 10
+
+for T in (Float32, Float64, Complex64, Complex128)
+
+    context("Matrix{$T}") do
+
+    A = rand(T, n, n)
+    A = A + A'
+
+    tol = (eltype(T) <: Complex ? 2 : 1) * n^2 * cond(A) * eps(real(one(T)))
     v = eigvals(A)
 
     ## Simple methods
 
     context("Power iteration") do
-    eval_big = maximum(v) > abs(minimum(v)) ? maximum(v) : minimum(v)
-    eval_pow = powm(A; tol=sqrt(eps(real(one(T)))), maxiter=2000)[1]
-    @fact norm(eval_big-eval_pow) --> less_than(tol)
+        eval_big = maximum(v) > abs(minimum(v)) ? maximum(v) : minimum(v)
+        eval_pow = powm(A; tol = sqrt(eps(real(one(T)))), maxiter = 2000)[1]
+        @fact norm(eval_big - eval_pow) --> less_than(tol)
     end
 
     context("Inverse iteration") do
-    irnd = ceil(Int, rand()*(n-2))
-    eval_rand = v[1+irnd] #Pick random eigenvalue
-    # Perturb the eigenvalue by < 1/4 of the distance to the nearest eigenvalue
-    eval_diff = min(abs(v[irnd]-eval_rand), abs(v[irnd+2]-eval_rand))
-    σ = eval_rand + eval_diff/2*(rand()-.5)
-    eval_ii = invpowm(A; shift=σ, tol=sqrt(eps(real(one(T)))), maxiter=2000)[1]
-    @fact norm(eval_rand-eval_ii) --> less_than(tol)
+        irnd = ceil(Int, rand() * (n - 2))
+        eval_rand = v[1 + irnd] #Pick random eigenvalue
+        # Perturb the eigenvalue by < 1/4 of the distance to the nearest eigenvalue
+        eval_diff = min(abs(v[irnd] - eval_rand), abs(v[irnd + 2] - eval_rand))
+        σ = eval_rand + eval_diff / 2 * (rand() - 0.5)
+
+        F = lufact(A - UniformScaling(σ))
+        Fmap = LinearMap((y, x) -> A_ldiv_B!(y, F, x), nothing, size(A, 1), size(A, 2), T, ismutating = true)
+
+        λ, x = invpowm(Fmap; shift = σ, tol = sqrt(eps(real(T))), maxiter = 2000, verbose = true)
+
+        @fact norm(A * x - λ * x) --> less_than(tol)
+        @fact norm(eval_rand - λ) --> less_than(tol)
+
     end
 
     end
