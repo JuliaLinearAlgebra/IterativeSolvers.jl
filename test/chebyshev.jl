556cea7de6547f38fcf86cd60e41f32df6a7a762
Harmen <harmenstoppels@gmail.com>
2017-07-14 16:29:39 +0200

    Improve tests: with preconditioner and a more sane eigenspectrum for the SPD matrix (no eigenvalues close to 0 to improve convergence)

--- test/chebyshev.jl
+++ test/chebyshev.jl
@@ -5,24 +5,41 @@ using LinearMaps
 
 srand(1234321)
 
+function randSPD(T, n)
+    A = rand(T, n, n) + n * eye(T, n)
+    A = A' + A
+    A = A' * A
+end
+
 #Chebyshev
 facts("Chebyshev") do
+n = 10
 for T in (Float32, Float64, Complex64, Complex128)
     context("Matrix{$T}") do
-    A=convert(Matrix{T}, randn(n,n))
-    T<:Complex && (A+=convert(Matrix{T}, im*randn(n,n)))
-    A=A+A'
-    A=A'*A #Construct SPD matrix
-    b=convert(Vector{T}, randn(n))
-    T<:Complex && (b+=convert(Vector{T}, im*randn(n)))
-    b=b/norm(b)
-    tol = 0.1 #For some reason Chebyshev is very slow
-    v = eigvals(A)
-    mxv = maximum(v)
-    mnv = minimum(v)
-    x_cheby, c_cheby= chebyshev(A, b, mxv+(mxv-mnv)/100, mnv-(mxv-mnv)/100, tol=tol, maxiter=10^5, log=true)
-    @fact c_cheby.isconverged --> true
-    @fact norm(A*x_cheby-b) --> less_than(tol)
+        A = randSPD(T, n)
+        b = rand(T, n)
+        b /= norm(b)
+
+        tol = sqrt(eps(real(T)))
+        mnv, mxv = eigmin(A), eigmax(A)
+        Δ = (mxv - mnv) / 100
+
+        x, history = chebyshev(A, b, mnv - Δ, mxv + Δ, tol=tol, maxiter=10n, log=true)
+        @fact history.isconverged --> true
+        @fact norm(A * x - b) --> less_than(tol)
+
+        context("Preconditioned") do
+            B = randSPD(T, n)
+            B_fact = cholfact!(B)
+            BA = B_fact \ A
+            λs = eigvals(BA)
+            mnv, mxv = minimum(real(λs)), maximum(real(λs))
+            Δ = (mxv - mnv) / 100
+
+            x, history = chebyshev(A, b, mnv - Δ, mxv + Δ, Pl = B_fact, tol=tol, maxiter=10n, log=true)
+            @fact history.isconverged --> true
+            @fact norm(A * x - b) --> less_than(tol)
+        end
     end
 end
 end
