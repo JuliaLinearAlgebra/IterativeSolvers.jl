fafe6b5b0087c4ee7079e315f941381a0b5995b0
Harmen Stoppels <harmenstoppels@gmail.com>
2017-07-25 13:28:21 +0200

    Use Base.Test in lsmr

--- test/lsmr.jl
+++ test/lsmr.jl
@@ -1,6 +1,8 @@
 using IterativeSolvers
-using FactCheck
 using Base.Test
+
+import Base: size, A_mul_B!, Ac_mul_B!, eltype, similar, scale!, copy!, fill!, length
+import Base.LinAlg: norm
 
 srand(1234321)
 
@@ -9,12 +11,12 @@ type Wrapper
     m::Int
     n::Int
 end
-Base.size(op::Wrapper, dim::Integer) = (dim == 1) ? op.m :
-                                            (dim == 2) ? op.n : 1
-Base.size(op::Wrapper) = (op.m, op.n)
-Base.eltype(op::Wrapper) = Int
 
-function Base.A_mul_B!(α, A::Wrapper, x, β, y)
+size(op::Wrapper, dim::Integer) = (dim == 1) ? op.m : (dim == 2) ? op.n : 1
+size(op::Wrapper) = op.m, op.n
+eltype(op::Wrapper) = Int
+
+function A_mul_B!(α, A::Wrapper, x, β, y)
     m, n = size(A)
     scale!(y, β)
     y[1] = y[1] + α * x[1]
@@ -27,21 +29,19 @@ function Base.A_mul_B!(α, A::Wrapper, x, β, y)
     return y
 end
 
-function Base.Ac_mul_B!(α, A::Wrapper, x, β, y)
+function Ac_mul_B!(α, A::Wrapper, x, β, y)
     m, n = size(A)
     mn = min(m, n)
     scale!(y, β)
-    for i = 1:mn-1
-        y[i] = y[i] + α * i* (x[i]+x[i+1])
+    for i = 1 : mn - 1
+        y[i] = y[i] + α * i * (x[i] + x[i + 1])
     end
     y[mn] = y[mn] + α * mn * x[mn]
-    for i = m+1:n
+    for i = m + 1 : n
         y[i] = y[i]
     end
     return y
 end
-
-
 
 # Type used in Dampenedtest
 # solve (A'A + diag(v).^2 ) x = b
@@ -50,38 +50,32 @@ type DampenedVector{Ty, Tx}
     y::Ty
     x::Tx
 end
-Base.eltype(a::DampenedVector) =  promote_type(eltype(a.y), eltype(a.x))
 
-function Base.norm(a::DampenedVector)
-    return sqrt(norm(a.y)^2 + norm(a.x)^2)
-end
+eltype(a::DampenedVector) = promote_type(eltype(a.y), eltype(a.x))
+norm(a::DampenedVector) = sqrt(norm(a.y)^2 + norm(a.x)^2)
 
-function Base.copy!{Ty, Tx}(a::DampenedVector{Ty, Tx}, b::DampenedVector{Ty, Tx})
+function copy!{Ty, Tx}(a::DampenedVector{Ty, Tx}, b::DampenedVector{Ty, Tx})
     copy!(a.y, b.y)
     copy!(a.x, b.x)
-    return a
+    a
 end
 
-function Base.fill!(a::DampenedVector, α::Number)
+function fill!(a::DampenedVector, α::Number)
     fill!(a.y, α)
     fill!(a.x, α)
-    return a
+    a
 end
 
-Base.scale!(α::Number, a::DampenedVector) = Base.scale!(a, α)
-function Base.scale!(a::DampenedVector, α::Number)
+scale!(α::Number, a::DampenedVector) = scale!(a, α)
+
+function scale!(a::DampenedVector, α::Number)
     scale!(a.y, α)
     scale!(a.x, α)
-    return a
+    a
 end
 
-function Base.similar(a::DampenedVector, T)
-    return DampenedVector(similar(a.y, T), similar(a.x, T))
-end
-
-function Base.length(a::DampenedVector)
-    length(a.y) + length(a.x)
-end
+similar(a::DampenedVector, T) = DampenedVector(similar(a.y, T), similar(a.x, T))
+length(a::DampenedVector) = length(a.y) + length(a.x)
 
 
 type DampenedMatrix{TA, Tx}
@@ -89,16 +83,16 @@ type DampenedMatrix{TA, Tx}
     diagonal::Tx
 end
 
-Base.eltype(A::DampenedMatrix) = promote_type(eltype(A.A), eltype(A.diagonal))
+eltype(A::DampenedMatrix) = promote_type(eltype(A.A), eltype(A.diagonal))
 
-function Base.size(A::DampenedMatrix, dim::Integer)
+function size(A::DampenedMatrix, dim::Integer)
     m, n = size(A.A)
     l = length(A.diagonal)
     dim == 1 ? (m + l) :
     dim == 2 ? n : 1
 end
 
-function Base.A_mul_B!{TA, Tx, Ty}(α::Number, mw::DampenedMatrix{TA, Tx}, a::Tx,
+function A_mul_B!{TA, Tx, Ty}(α::Number, mw::DampenedMatrix{TA, Tx}, a::Tx,
                 β::Number, b::DampenedVector{Ty, Tx})
     if β != 1.
         if β == 0.
@@ -112,7 +106,7 @@ function Base.A_mul_B!{TA, Tx, Ty}(α::Number, mw::DampenedMatrix{TA, Tx}, a::Tx
     return b
 end
 
-function Base.Ac_mul_B!{TA, Tx, Ty}(α::Number, mw::DampenedMatrix{TA, Tx}, a::DampenedVector{Ty, Tx},
+function Ac_mul_B!{TA, Tx, Ty}(α::Number, mw::DampenedMatrix{TA, Tx}, a::DampenedVector{Ty, Tx},
                 β::Number, b::Tx)
     if β != 1.
         if β == 0.
@@ -126,20 +120,16 @@ function Base.Ac_mul_B!{TA, Tx, Ty}(α::Number, mw::DampenedMatrix{TA, Tx}, a::D
     return b
 end
 
+@testset "LSMR" begin
 
-
-facts(string("lsmr")) do
-
-
-    context("Small dense matrix") do
+    @testset "Small dense matrix" begin
         A = rand(10, 5)
         b = rand(10)
         x = lsmr(A, b)
-        @fact norm(x - A\b) --> less_than(√eps())
+        @test norm(x - A\b) ≤ √eps()
     end
 
-
-    context("SOL test") do
+    @testset "SOL test" for (m, n, damp) = ((10, 10, 0), (20, 10, 0), (20, 10, 0.1))
         # Test adapted from the BSD-licensed Matlab implementation at
         #    http://www.stanford.edu/group/SOL/software/lsqr.html
         #              Michael Saunders, Systems Optimization Laboratory,
@@ -159,35 +149,25 @@ facts(string("lsmr")) do
         # 11 Apr 1996: First version for distribution with lsqr.m.
         #              Michael Saunders, Dept of EESOR, Stanford University.
 
-        function SOLtest(m, n, damp)
-            A = Wrapper(m, n)
-            xtrue = n:-1:1
-            b = Array(Float64, m)
-            b = float(A_mul_B!(1.0, A, xtrue, 0.0, b))
-            x = lsmr(A, b, atol = 1e-7, btol = 1e-7, conlim = 1e10, maxiter = 10n)
-            r = A_mul_B!(-1, A, x, 1, b)
-            @fact norm(r) --> less_than_or_equal(1e-4)
-        end
-        SOLtest(10, 10, 0)
-        SOLtest(20, 10, 0)
-        SOLtest(20, 10, 0.1)
+        A = Wrapper(m, n)
+        xtrue = n : -1 : 1
+        b = Vector{Float64}(m)
+        A_mul_B!(1.0, A, xtrue, 0.0, b)
+        x = lsmr(A, b, atol = 1e-7, btol = 1e-7, conlim = 1e10, maxiter = 10n)
+        r = A_mul_B!(-1, A, x, 1, b)
+        @test norm(r) ≤ 1e-4
     end
 
-
-
-    context("dampened test") do
+    @testset "Dampened test" for (m, n) = ((10, 10), (20, 10))
         # Test used to make sure A, b can be generic matrix / vector
         srand(1234)
-        function DampenedTest(m, n)
-            b = rand(m)
-            A = rand(m, n)
-            v = rand(n)
-            Adampened = DampenedMatrix(A, v)
-            bdampened = DampenedVector(b, zeros(n))
-            x, ch = lsmr(Adampened, bdampened, log=true)
-            @fact norm((A'A + diagm(v).^2)x - A'b) --> less_than(1e-3)
-        end
-        DampenedTest(10, 10)
-        DampenedTest(20, 10)
+        
+        b = rand(m)
+        A = rand(m, n)
+        v = rand(n)
+        Adampened = DampenedMatrix(A, v)
+        bdampened = DampenedVector(b, zeros(n))
+        x, ch = lsmr(Adampened, bdampened, log=true)
+        @test norm((A'A + diagm(v) .^ 2)x - A'b) ≤ 1e-3
     end
 end
