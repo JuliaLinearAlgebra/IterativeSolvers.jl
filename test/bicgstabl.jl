ce0ed8ffbcf6e2fc6d145c393ed579cf646a7c8f
Harmen Stoppels <harmenstoppels@gmail.com>
2017-07-25 11:43:12 +0200

    Use Base.Test in bicgstab

--- test/bicgstabl.jl
+++ test/bicgstabl.jl
@@ -1,34 +1,29 @@
 using IterativeSolvers
-using FactCheck
 using LinearMaps
+using Base.Test
 
 srand(1234321)
 
 include("advection_diffusion.jl")
 
-facts("bicgstab(l)") do
-
-for T in (Float32, Float64, Complex64, Complex128)
-    context("Matrix{$T}") do
-
+@testset ("BiCGStab(l)") begin
+    @testset "Matrix{$T}" for T in (Float32, Float64, Complex64, Complex128)
         n = 20
         A = rand(T, n, n) + 15 * eye(T, n)
         x = ones(T, n)
         b = A * x
 
-        for l = (2, 4)
-            context("BiCGStab($l)") do
-
+        tol = √eps(real(T))
+  
+        @testset "BiCGStab($l)" for l = (2, 4)
             # Solve without preconditioner
-            x1, his1 = bicgstabl(A, b, l, max_mv_products = 100, log = true)
-            @fact norm(A * x1 - b) / norm(b) --> less_than(√eps(real(one(T))))
+            x1, his1 = bicgstabl(A, b, l, max_mv_products = 100, log = true, tol = tol)
+            @test norm(A * x1 - b) / norm(b) ≤ tol
 
             # Do an exact LU decomp of a nearby matrix
             F = lufact(A + rand(T, n, n))
-            x2, his2 = bicgstabl(A, b, Pl = F, l, max_mv_products = 100, log = true)
-            @fact norm(A * x2 - b) / norm(b) --> less_than(√eps(real(one(T))))
-            end
+            x2, his2 = bicgstabl(A, b, Pl = F, l, max_mv_products = 100, log = true, tol = tol)
+            @test norm(A * x2 - b) / norm(b) ≤ tol
         end
     end
-end
 end
