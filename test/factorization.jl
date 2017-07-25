a39d48a8fda5fbf332a972a054aecb6dc8f3eeac
Harmen Stoppels <harmenstoppels@gmail.com>
2017-07-25 12:20:12 +0200

    Move factorization to Base.Test

--- test/factorization.jl
+++ test/factorization.jl
@@ -1,11 +1,11 @@
 using IterativeSolvers
-using FactCheck
+using Base.Test
 
-facts("idfact") do
+@testset "IDfact" begin
     srand(1)
     M = randn(4,5)
     k = 3
 
     F = idfact(M, k, 3)
-    @fact vecnorm(F.B*F.P-M) --> less_than_or_equal(2svdvals(M)[k+1])
+    @test vecnorm(F.B * F.P - M) ≤ 2svdvals(M)[k + 1]
 end
