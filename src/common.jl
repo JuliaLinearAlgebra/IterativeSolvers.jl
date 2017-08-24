62b1a0417c273a46f63b5145ea8d1bab7cdb60cf
Harmen Stoppels <harmenstoppels@gmail.com>
2017-08-24 14:32:54 +0200

    Remove unused methods

--- src/common.jl
+++ src/common.jl
@@ -13,22 +13,6 @@ Determine type of the division of an element of `b` against an element of `A`:
 Adivtype(A, b) = typeof(one(eltype(b))/one(eltype(A)))
 
 """
-    Amultype(A, x)
-Determine type of the multiplication of an element of `b` with an element of `A`:
-`typeof(one(eltype(A))*one(eltype(x)))`
-"""
-Amultype(A, x) = typeof(one(eltype(A))*one(eltype(x)))
-
-"""
-    randx(A, b)
-Build a random unitary vector `Vector{T}`, where `T` is `Adivtype(A,b)`.
-"""
-function randx(A, b)
-    T = Adivtype(A, b)
-    x = initrand!(Array(T, size(A, 2)))
-end
-
-"""
     zerox(A, b)
 Build a zeros vector `Vector{T}`, where `T` is `Adivtype(A,b)`.
 """
@@ -43,28 +27,10 @@ end
 Solve `A\b` with a direct solver. When `A` is a function `A(b)` is dispatched instead.
 """
 solve(A::Function,b) = A(b)
-
 solve(A,b) = A\b
-
 solve!(out::AbstractArray{T},A::Int,b::AbstractArray{T}) where {T} = scale!(out,b, 1/A)
-
 solve!(out::AbstractArray{T},A,b::AbstractArray{T}) where {T} = A_ldiv_B!(out,A,b)
 solve!(out::AbstractArray{T},A::Function,b::AbstractArray{T}) where {T} = copy!(out,A(b))
-
-"""
-    initrand!(v)
-Overwrite `v` with a random unitary vector of the same length.
-"""
-function initrand!(v::Vector)
-    _randn!(v)
-    nv = norm(v)
-    for i = 1:length(v)
-        v[i] /= nv
-    end
-    v
-end
-_randn!(v::Array{Float64}) = randn!(v)
-_randn!(v) = copy!(v, randn(length(v)))
 
 # Identity preconditioner
 struct Identity end
