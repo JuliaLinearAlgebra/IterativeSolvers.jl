8ed1347b66e449e4e84c85f619ab17b3df6d1d13
Harmen Stoppels <harmenstoppels@gmail.com>
2017-12-06 11:23:24 +0100

    Add tests for convergence history plot recipes

--- test/history.jl
+++ test/history.jl
@@ -1,5 +1,68 @@
 using IterativeSolvers
+using RecipesBase
+using Base.Test
 
 @testset "ConvergenceHistory" begin
 
+    const KW = Dict{Symbol, Any}
+
+    RecipesBase.is_key_supported(k::Symbol) = k == :sep ? false : true
+
+    # No plottables
+    begin
+        history = ConvergenceHistory(partial = false)
+        @test_throws ErrorException RecipesBase.apply_recipe(KW(), history)
+    end
+
+    # Without restart markers
+    begin
+        history = ConvergenceHistory(partial = false)
+        history.iters = 3
+        history.data[:resnorm] = [10.0, 3.0, 0.1]
+        
+        plots = (
+            RecipesBase.apply_recipe(KW(), history),
+            RecipesBase.apply_recipe(KW(), history, :resnorm)
+        )
+
+        for data in plots
+            @test length(data) == 1
+            @test data[1].d[:label] == "resnorm"
+            @test data[1].d[:seriestype] == :line
+        end
+    end
+
+    # With restart markers
+    begin
+        history = ConvergenceHistory(partial = false, restart = 2)
+        history.iters = 3
+        history.data[:resnorm] = [10.0, 3.0, 0.1]
+
+        plots = (
+            RecipesBase.apply_recipe(KW(), history),
+            RecipesBase.apply_recipe(KW(), history, :resnorm)
+        )
+
+        for data in plots
+            @test length(data) == 2
+            @test data[2].d[:linecolor] == :white    
+        end
+    end
+
+    # Custom color
+    begin
+        history = ConvergenceHistory(partial = false, restart = 2)
+        history.iters = 3
+        history.data[:resnorm] = [10.0, 3.0, 0.1]
+
+        plots = (
+            RecipesBase.apply_recipe(KW(:sep => :red), history),
+            RecipesBase.apply_recipe(KW(:sep => :red), history, :resnorm)
+        )
+
+        for data in plots
+            @test length(data) == 2
+            @test data[2].d[:linecolor] == :red
+        end
+    end
 end
