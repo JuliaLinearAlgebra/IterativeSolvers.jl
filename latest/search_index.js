cc2c6d3fc07646df4f4f3b78303d3af702cfe9da
autodocs <autodocs>
2017-12-09 19:51:15 +0000

    build based on 9383e1d

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "IterativeSolvers.jl",
     "category": "section",
-    "text": "IterativeSolvers.jl is a Julia package that provides efficient iterative algorithms for solving large linear systems, eigenproblems, and singular value problems. Most of the methods can be used matrix-free.For bug reports, feature requests and questions please submit an issue. If you're interested in contributing, please see the Contributing guide.For more information on future methods have a look at the package roadmap on deterministic methods, for randomized algorithms check here."
+    "text": "IterativeSolvers.jl is a Julia package that provides efficient iterative algorithms for solving large linear systems, eigenproblems, and singular value problems. Most of the methods can be used matrix-free.For bug reports, feature requests and questions please submit an issue. If you're interested in contributing, please see the Contributing guide.For more information on future methods have a look at the package roadmap."
 },
 
 {
@@ -30,14 +30,6 @@ var documenterSearchIndex = {"docs": [
     "title": "Eigenproblems and SVD",
     "category": "section",
     "text": "For the Singular Value Decomposition we offer SVDL, which is the Golub-Kahan-Lanczos procedure.For eigenvalue problems we have at this point just the Power Method and some convenience wrappers to do shift-and-invert."
-},
-
-{
-    "location": "index.html#Randomized-algorithms-1",
-    "page": "Home",
-    "title": "Randomized algorithms",
-    "category": "section",
-    "text": "Randomized algorithms have gotten some traction lately. Some of the methods mentioned in [Halko2011] have been implemented as well, although their quality is generally poor. Also note that many classical methods such as the subspace iteration, BiCG and recent methods like IDR(s) are also \"randomized\" in some sense.[Halko2011]: Halko, Nathan, Per-Gunnar Martinsson, and Joel A. Tropp. \"Finding structure with randomness: Probabilistic algorithms for constructing approximate matrix decompositions.\" SIAM review 53.2 (2011): 217-288."
 },
 
 {
@@ -742,110 +734,6 @@ var documenterSearchIndex = {"docs": [
     "title": "Implementation details",
     "category": "section",
     "text": "The implementation of thick restarting follows closely that of SLEPc as described in [Hernandez2008]. Thick restarting can be turned off by setting k = maxiter, but most of the time this is not desirable.The singular vectors are computed directly by forming the Ritz vectors from the product of the Lanczos vectors L.P/L.Q and the singular vectors of L.B. Additional accuracy in the singular triples can be obtained using inverse iteration.[Hernandez2008]: Vicente Hernández, José E. Román, and Andrés Tomás. \"A robust and efficient parallel SVD solver based on restarted Lanczos bidiagonalization.\" Electronic Transactions on Numerical Analysis 31 (2008): 68-85."
-},
-
-{
-    "location": "randomized.html#",
-    "page": "Randomized algorithms",
-    "title": "Randomized algorithms",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.reig",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.reig",
-    "category": "Function",
-    "text": "reig(A, l)\n\nCompute the spectral (Eigen) decomposition of A using a randomized algorithm.\n\nArguments\n\nA: input matrix.\nl::Int: number of eigenpairs to find.\n\nOutput\n\n::Base.LinAlg.Eigen: eigen decomposition.\n\nImplementation note\n\nThis is a wrapper around eigfact_onepass() which uses the randomized samples found using srft(l).\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.rsvdfact",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.rsvdfact",
-    "category": "Function",
-    "text": "rsvdfact(A, n, p=0)\n\nCompute partial singular value decomposition of A using a randomized algorithm.\n\nArguments\n\nA: input matrix.\n\nn::Int: number of singular value/vector pairs to find.\n\np::Int=0: number of extra vectors to include in computation.\n\nOutput\n\n::SVD: singular value decomposition.\n\nwarning: Accuracy\nThis variant of the randomized singular value decomposition is the most commonly found implementation but is not recommended for accurate computations, as it often has trouble finding the n largest singular pairs, but rather finds n large singular pairs which may not necessarily be the largest.\n\nImplementation note\n\nThis function calls rrange, which uses naive randomized rangefinding to compute a basis for a subspace of dimension n (Algorithm 4.1 of [Halko2011]), followed by svdfact_restricted(), which computes the exact SVD factorization on the restriction of A to this randomly selected subspace (Algorithm 5.1 of [Halko2011]).\n\nAlternatively, you can mix and match your own randomized algorithm using any of the randomized range finding algorithms to find a suitable subspace and feeding the result to one of the routines that computes the SVD restricted to that subspace.\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.rsvd_fnkz",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.rsvd_fnkz",
-    "category": "Function",
-    "text": "rsvd_fnkz(A, k)\n\nCompute the randomized SVD by iterative refinement from randomly selected columns/rows [Friedland2006].\n\nArguments\n\nA: matrix whose SVD is desired;\nk::Int: desired rank of approximation (k ≤ min(m, n)).\n\nKeywords\n\nl::Int = k: number of columns/rows to sample at each iteration (1 ≤ l ≤ k);\nN::Int = minimum(size(A)): maximum number of iterations;\nϵ::Real = prod(size(A))*eps(): relative threshold for convergence, as measured by growth of the spectral norm;\nmethod::Symbol = :eig: problem to solve.\n:eig: eigenproblem.\n:svd: singular problem.\nverbose::Bool = false: print convergence information at each iteration.\n\nReturn value\n\nSVD object of rank ≤ k.\n\n[Friedland2006]: Friedland, Shmuel, et al. \"Fast Monte-Carlo low rank approximations for  matrices.\" System of Systems Engineering, 2006 IEEE/SMC International  Conference on. IEEE, 2006.\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#Randomized-1",
-    "page": "Randomized algorithms",
-    "title": "Randomized algorithms",
-    "category": "section",
-    "text": "The methods below are based on [Halko2011].reig\nrsvdfact\nrsvd_fnkz"
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.rcond",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.rcond",
-    "category": "Function",
-    "text": "rcond(A, iters=1)\n\nEstimate matrix condition number randomly.\n\nArguments\n\nA: matrix whose condition number to estimate. Must be square and\n\nsupport premultiply (A*⋅) and solve (A\\⋅).\n\niters::Int = 1: number of power iterations to run.\n\nKeywords\n\np::Real = 0.05: probability that estimate fails to hold as an upper bound.\n\nOutput\n\nInterval (x, y) which contains κ(A) with probability 1 - p.\n\nImplementation note\n\n[Dixon1983] originally describes this as a computation that can be done by computing the necessary number of power iterations given p and the desired accuracy parameter θ=y/x. However, these bounds were only derived under the assumptions of exact arithmetic. Empirically, iters≥4 has been seen to result in incorrect results in that the computed interval does not contain the true condition number. This implemention therefore makes iters an explicitly user-controllable parameter from which to infer the accuracy parameter and hence the interval containing κ(A). ```\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#Condition-number-estimate-1",
-    "page": "Randomized algorithms",
-    "title": "Condition number estimate",
-    "category": "section",
-    "text": "rcond"
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.reigmin",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.reigmin",
-    "category": "Function",
-    "text": "reigmin(A, iters=1)\n\nEstimate minimal eigenvalue randomly.\n\nArguments\n\nA: Matrix whose maximal eigenvalue to estimate.\n\nMust be square and support premultiply (A*⋅).\n\niters::Int=1: Number of power iterations to run. (Recommended: iters ≤ 3)\n\nKeywords\n\np::Real=0.05: Probability that estimate fails to hold as an upper bound.\n\nOutput\n\nInterval (x, y) which contains the maximal eigenvalue of A with probability 1 - p.\n\nReferences\n\nCorollary of Theorem 1 in [Dixon1983]\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.reigmax",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.reigmax",
-    "category": "Function",
-    "text": "reigmax(A, iters=1)\n\nEstimate maximal eigenvalue randomly.\n\nArguments\n\nA: Matrix whose maximal eigenvalue to estimate.\n\nMust be square and support premultiply (A*⋅).\n\niters::Int=1: Number of power iterations to run. (Recommended: iters ≤ 3)\n\nKeywords\n\np::Real=0.05: Probability that estimate fails to hold as an upper bound.\n\nOutput\n\nInterval (x, y) which contains the maximal eigenvalue of A with probability 1 - p.\n\nReferences\n\nCorollary of Theorem 1 in [Dixon1983]\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#Extremal-eigenvalue-estimates-1",
-    "page": "Randomized algorithms",
-    "title": "Extremal eigenvalue estimates",
-    "category": "section",
-    "text": "reigmin\nreigmax"
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.rnorm",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.rnorm",
-    "category": "Function",
-    "text": "rnorm(A, mvps)\n\nCompute a probabilistic upper bound on the norm of a matrix A. ‖A‖ ≤ α √(2/π) maxᵢ ‖Aωᵢ‖ with probability p=α^(-mvps).\n\nArguments\n\nA: matrix whose norm to estimate.\nmvps::Int: number of matrix-vector products to compute.\n\nKeywords\n\np::Real=0.05: probability of upper bound failing.\n\nOutput\n\nEstimate of ‖A‖.\n\nSee also rnorms for a different estimator that uses  premultiplying by both A and A'.\n\nReferences\n\nLemma 4.1 of Halko2011\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#IterativeSolvers.rnorms",
-    "page": "Randomized algorithms",
-    "title": "IterativeSolvers.rnorms",
-    "category": "Function",
-    "text": "rnorms(A, iters=1)\n\nEstimate matrix norm randomly using A'A.\n\nCompute a probabilistic upper bound on the norm of a matrix A.\n\nρ = √(‖(A'A)ʲω‖/‖(A'A)ʲ⁻¹ω‖)\n\nwhich is an estimate of the spectral norm of A produced by iters steps of the power method starting with normalized ω, is a lower bound on the true norm by a factor\n\nρ ≤ α ‖A‖\n\nwith probability greater than 1 - p, where p = 4\\sqrt(n/(iters-1)) α^(-2iters).\n\nArguments\n\nA: matrix whose norm to estimate.\niters::Int = 1: mumber of power iterations to perform.\n\nKeywords\n\np::Real = 0.05: probability of upper bound failing.\nAt = A': Transpose of A.\n\nOutput\n\nEstimate of ‖A‖.\n\nSee also rnorm for a different estimator that does not require premultiplying by A'\n\nReferences\n\nAppendix of [Liberty2007].\n\n\n\n"
-},
-
-{
-    "location": "randomized.html#Norm-estimate-1",
-    "page": "Randomized algorithms",
-    "title": "Norm estimate",
-    "category": "section",
-    "text": "rnorm\nrnorms[Halko2011]: Halko, Nathan, Per-Gunnar Martinsson, and Joel A. Tropp. \"Finding structure with randomness: Probabilistic algorithms for constructing approximate matrix decompositions.\" SIAM review 53.2 (2011): 217-288.[Dixon1983]: Dixon, John D. \"Estimating extremal eigenvalues and condition numbers of matrices.\" SIAM Journal on Numerical Analysis 20.4 (1983): 812-814.[Liberty2007]: Liberty, Edo, et al. \"Randomized algorithms for the low-rank approximation of matrices.\" Proceedings of the National Academy of Sciences 104.51 (2007): 20167-20172."
 },
 
 {
