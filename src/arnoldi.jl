5e43b3f44b3e25c4a6568a6b2592b365adf96894
Ramit-Pahwa <jarvis@Ramits-MacBook-Pro.local>
2018-03-09 22:08:32 +0530

    Fixed undef error

--- src/arnoldi.jl
+++ src/arnoldi.jl
@@ -3,7 +3,7 @@ function arnoldi_procedure(A,m::Int64)
     # Procedure to generate orthogonal basis of the Krylov subspace
     n=size(A,1)
     q=Matrix(m+1,n)
-    h=Matrix(m+1,m)
+    h=zeros(m+1,m)
     v=rand(n)
     w=Vector(n)
     q[1,:] = normalize(v)
@@ -17,7 +17,7 @@ function arnoldi_procedure(A,m::Int64)
         end
         h[j+1,j] = norm(w,1)
         if h[j+1,j] == 0 # Found an Invariant Subspace
-            return q,h
+            return q',h
         end
         q[j+1,:] = w/h[j+1,j]
     end
