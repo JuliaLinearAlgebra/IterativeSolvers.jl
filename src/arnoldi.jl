a1f909a5935c218a39f35a7557ccd49376dddf24
Ramit-Pahwa <jarvis@Ramits-MacBook-Pro.local>
2018-03-09 21:34:02 +0530

    Fixed

--- src/arnoldi.jl
+++ src/arnoldi.jl
@@ -1,16 +1,19 @@
-# function for arnoldi method 
 
-function arnoldi_procedure(A::StridedMatrix{T}, w::StridedVector{T}, h::StridedMatrix{T}, v::StridedVector{T},m::Int64) where {T}
+function arnoldi_procedure(A,m::Int64)
     # Procedure to generate orthogonal basis of the Krylov subspace
-    q = Matrix(m,size(v))
+    n=size(A,1)
+    q=Matrix(m+1,n)
+    h=Matrix(m+1,m)
+    v=rand(n)
+    w=Vector(n)
     q[1,:] = normalize(v)
     for j = 1:m
         A_mul_B!(w,A,q[j,:])
-        
+
         # Gram-Schmidt Orthogonalization
         for i = 1:j
-            h[i,j] = A_mul_B(w',q[i,:])
-            w = w-q[i,:]*h[i,j]     
+            h[i,j]=w'*q[i,:]
+            w = w-q[i,:]*h[i,j]
         end
         h[j+1,j] = norm(w,1)
         if h[j+1,j] == 0 # Found an Invariant Subspace
@@ -18,5 +21,5 @@ function arnoldi_procedure(A::StridedMatrix{T}, w::StridedVector{T}, h::StridedM
         end
         q[j+1,:] = w/h[j+1,j]
     end
-    return q,h
+    return q',h
 end
