01c8c058f9dcab55344fcc7b3f922a20361e4055
autodocs <autodocs>
2018-12-02 20:17:44 +0000

    build based on bb9553c

--- versions.js
+++ versions.js
@@ -1,12 +1,8 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-0.7",
-  "release-0.6",
-  "release-0.5",
-  "release-0.4",
-  "v0.7.1",
-  "v0.6.0",
-  "v0.5.0",
-  "v0.4.2",
+  "v0.7",
+  "v0.6",
+  "v0.5",
+  "v0.4",
+  "dev",
 ];
