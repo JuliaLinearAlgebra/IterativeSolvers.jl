8c9ae8a8aee84bfa4974c21cfb134c11ab265870
Kiran Shila <kiranshila@mail.usf.edu>
2019-02-23 22:13:52 -0500

    More boilerplate

--- src/qmr.jl
+++ src/qmr.jl
@@ -1,3 +1,54 @@
 export qmr, qmr!
 
 using LinearAlgebra
+
+"""
+    qmr(A, b; kwargs...) -> x, [history]
+
+Same as [`qmr!`](@ref), but allocates a solution vector `x` initialized with zeros.
+"""
+qmr(A, b; kwargs...) = qmr!(zerox(A, b), A, b; initially_zero = true, kwargs...)
+
+"""
+    qmr!(x, A, b; kwargs...) -> x, [history]
+
+Solves the problem ``Ax = b`` with the Quasi-Minimal Residual method.
+
+# Arguments
+
+- `x`: Initial guess, will be updated in-place;
+- `A`: linear operator;
+- `b`: right-hand side.
+
+## Keywords
+
+- `initially_zero::Bool`: If `true` assumes that `iszero(x)` so that one
+  matrix-vector product can be saved when computing the initial
+  residual vector;
+- `tol`: relative tolerance;
+- `Pl`: left preconditioner;
+- `Pr`: right preconditioner;
+- `log::Bool`: keep track of the residual norm in each iteration;
+- `verbose::Bool`: print convergence information during the iterations.
+
+# Return values
+
+**if `log` is `false`**
+
+- `x`: approximate solution.
+
+**if `log` is `true`**
+
+- `x`: approximate solution;
+- `history`: convergence history.
+"""
+function qmr!(x, A, b;
+  Pl = Identity(),
+  Pr = Identity(),
+  tol = sqrt(eps(real(eltype(b)))),
+  log::Bool = false,
+  initially_zero::Bool = false,
+  verbose::Bool = false)
+
+
+end
