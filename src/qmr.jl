ee47ec5beac4db39e58a0a1fd8b47eaf487abba9
Kiran Shila <kiranshila@mail.usf.edu>
2019-02-28 01:00:45 -0500

    First pass read of algorithm, added test, not currently working

--- src/qmr.jl
+++ src/qmr.jl
@@ -12,7 +12,7 @@ qmr(A, b; kwargs...) = qmr!(zerox(A, b), A, b; initially_zero = true, kwargs...)
 """
     qmr!(x, A, b; kwargs...) -> x, [history]
 
-Solves the problem ``Ax = b`` with the Quasi-Minimal Residual method.
+Solves the problem ``Ax = b`` with the Quasi-Minimal Residual method without lookahead.
 
 # Arguments
 
@@ -44,13 +44,108 @@ Solves the problem ``Ax = b`` with the Quasi-Minimal Residual method.
 - `history`: convergence history.
 """
 function qmr!(x,A, b;
-  Pl = Identity(),
-  Pr = Identity(),
+  Pl = I,
+  Pr = I,
   tol = sqrt(eps(real(eltype(b)))),
   maxiter = size(A, 2),
   log::Bool = false,
   initially_zero::Bool = false,
   verbose::Bool = false)
 
+  history = ConvergenceHistory(partial = !log)
+  history[:tol] = tol
+  reserve!(history, :resnorm, maxiter)
 
+  δ = []
+  p = []
+  q = []
+  ϵ = []
+  β = []
+  θ = []
+
+  r = b-A*x
+  ν_vec = [zeros((length(r),1)),r]
+  y = Pl \ ν_vec[2]
+  ρ = [0,norm(y,2)]
+  w_vec = [zeros((length(r),1)),r]
+  z =  Pr' \ w_vec[2]
+  ζ = [0,norm(z,2)]
+
+  γ = [1]
+  η = [-1]
+
+  for i in 2:maxiter
+
+    if ρ[i] == 0 || ζ[1] == 0
+      history.isconverged = false;
+      break
+    end
+
+    ν[i] = ν_vec[i]/ρ[i];y = y / ρ[i]
+    w[i] = w_vec[i]/ζ[i];z = z / ζ[i]
+    δ[i] = z'*y
+
+    if δ[i] == 0
+      history.isconverged = false;
+      break
+    end
+
+    y_vec = Pr \ y
+    z_vec = Pl' \ z
+
+    if i == 2
+      p[2] = y_vec; q[2] = z_vec
+    else
+      p[i] = y_vec - ((ζ[i]*δ[i])/(ϵ[i-1]))*p[i-1]
+      q[i] = z_vec - ((ρ[i]*δ[i])/(ϵ[i-1]))*q[i-1]
+    end
+
+    p_vec = A*p[i]
+    ϵ[i] = q[i]'*p_vec
+
+    if ϵ[i] == 0
+      history.isconverged = false;
+      break;
+    end
+
+    β[i] = ϵ[i] / δ[i]
+
+    if β[i] == 0
+      history.isconverged = false;
+      break;
+    end
+
+    ν_vec[i+1] = p_vec - β[i]*ν[i]
+    y = ν_vec[i+1] \ Pl
+    ρ[i+1] = norm(y,2)
+    w_vec[i+1] = A'*q[i] - β[i]*w[i]
+    z = w_vec[i+1] \ Pr'
+    ζ[i+1] = norm(z,2)
+    θ[i] = ρ[i+1] / (γ[i-1] * abs(β[i]))
+    γ[i] = 1 / sqrt(1 + θ[i]^2)
+
+    if γ[i] == 0
+      history.isconverged = false;
+      break;
+    end
+
+    η[i] = (-η[i-1]*ρ[i]*γ[i]^2)/(β[i]*γ[i-1]^2)
+
+    if i == 2
+      d[2] = η[2]*p[2]; s[2] = η[2]*p_vec
+    else
+      d[i] = η[i]*p[i] + (θ[i-1]*γ[i])^2*d[i-1]
+      s[i] = η[i]*p_vec + (θ[i-1]*γ[i])^2*s[i-1]
+    end
+
+    x[i] = x[i-1] + d[i]
+    r[i] = r[i-1] - s[i]
+    push!(history, :resnorm, r[i])
+    if r[i] <= tol
+      history.isconverged = true;
+      break;
+    end
+  end
+
+  log ? (x,history) :  x
 end
--- test/qmr.jl
+++ test/qmr.jl
@@ -3,3 +3,32 @@ using Test
 using LinearAlgebra
 using SparseArrays
 using Random
+
+@testset "QMR" begin
+
+function matlab_example(T,n)
+    A = spdiagm(-1 => fill(-1,n-1), 1 => fill(4,n-1))
+    b = sum(A,dims=2)
+    M1 = spdiagm(-1 => fill(-1/2,n-1), 0 => ones(n))
+    M2 = spdiagm(0 => fill(4,n), 1 => fill(-1,n-1))
+    x = ones(T,100)
+    A,x,b
+end
+
+Random.seed!(123)
+
+@testset "MATLAB Example Matrix{$T}" for T in (Float32, Float64, ComplexF32, ComplexF64)
+    A, x, b = matlab_example(T, n)
+    tol = 1e-8
+
+    x0 = rand(T,n)
+
+    x1, hist1 = qmr(A, b, maxiter = 15, tol = tol, log = true)
+    x2, hist2 = qmr!(x0, A, b, maxiter = 15, tol = tol, log = true)
+
+    @test isa(hist1, ConvergenceHistory)
+    @test norm(b - A * x1) / norm(b) ≤ tol
+    @test hist1.isconverged
+    @test norm(b - A * x2) / norm(b) ≤ tol
+    @test x2 == x0
+end
