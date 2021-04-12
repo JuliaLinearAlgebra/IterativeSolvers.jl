48001292f565159ae204e74a222799122816a990
Wonseok Shin <wsshin97@gmail.com>
2021-04-12 12:27:10 -0400

    Remove unnecessary comments

--- src/common.jl
+++ src/common.jl
@@ -42,4 +42,4 @@ _norm(x, ::ConjugatedDot) = norm(x)
 _dot(x, y, ::ConjugatedDot) = dot(x, y)
 
 _norm(x, ::UnconjugatedDot) = sqrt(sum(xₖ->xₖ^2, x))
-_dot(x, y, ::UnconjugatedDot) = transpose(@view(x[:])) * @view(y[:])  # allocating, but faster than sum(prod, zip(x,y))
+_dot(x, y, ::UnconjugatedDot) = transpose(@view(x[:])) * @view(y[:])
